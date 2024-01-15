cf754a61be9569bb4d2424c68e994211ae771ef3
Ankush2107 <ankush.kumar210701@gmail.com>
2024-01-15 10:35:41 +0530

    seggregating the function inside todolist

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -4,26 +4,37 @@ const Todo = ({ todoData, isFinished, changeFinished, onDelete, onEdit }) => {
     const [ finished, setFinished ] = useState(isFinished);
     const [ isEditting, setIsEditting ] = useState(false);
     const [ editText, setEditText ] = useState(todoData);
+
+    const handleCheckboxChange = (e) => {
+      setFinished(e.target.checked); // superficially
+      changeFinished(e.target.checked); // Internally
+    };
+
+    const handleEditToggle = () => {
+      setIsEditting(!isEditting);
+      if (isEditting) {
+        onEdit(editText);
+      }
+    };
+
+    const handleInputChange = (e) => {
+      setEditText(e.target.value);
+    };
+
   return (
     <div>
-        <input type="checkbox" checked={finished} onChange={(e) => {
-            setFinished(e.target.checked) // Updates the local finished state with the new checked state of the checkbox I mean superficially.
-            changeFinished(e.target.checked) // Internally updates the existing finished value.
-        }}  
-        />
+        <input type="checkbox" checked={finished} onChange={handleCheckboxChange} />
 
         {/* TodoData */}
         {(isEditting) ? <input type="text" 
                                value={editText} 
-                               onChange={(e) => setEditText(e.target.value)} 
+                               onChange={handleInputChange} 
                         /> : <span>{todoData}</span> }
  
-        <button onClick={() => {setIsEditting(!isEditting)
-                                onEdit(editText)
-                                }}>{(!isEditting) ? 'Edit' : 'Save'}
-        </button>
+        <button onClick={handleEditToggle}>{(!isEditting) ? 'Edit' : 'Save'}</button>
 
         <button onClick={onDelete}>Delete</button>
+
     </div>
   )
 }
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -4,43 +4,47 @@ import { useContext } from "react";
 
 const TodoList = () => {
     const {list, setList} = useContext(TodoContext);
+
+    function onFinished(todo, isFinished) {
+      const updatedList = list.map(t => {
+        if(t.id == todo.id) {
+            todo.finished = isFinished;
+        }
+        return t;
+      });
+      setList(updatedList);
+    }
+
+    function onDelete(todo) {
+      const updatedList = list.filter(t => t.id != todo.id) 
+      setList(updatedList);
+    }
+
+    function onEdit(todo, todoText) {
+      const updatedList = list.map(t => {
+        if(t.id == todo.id) {
+            todo.todoData = todoText;
+        }
+        return t;
+      });
+      setList(updatedList);
+    }
+    
   return (
     <div>
       { list.length > 0 && list.map((todo) => (
             <Todo 
-                key={todo.id} 
-                id={todo.id} 
-                todoData={todo.todoData} 
-                isFinished={todo.finished} 
+              key={todo.id} 
+              id={todo.id} 
+              todoData={todo.todoData} 
+              isFinished={todo.finished} 
 
                 /*
                     changeFinished is used to update the finished state of the corresponding todo in the list and then calls updateList to update the overall todo list in the parent component.
                 */  
-                
-                changeFinished={(isFinished) => {
-                    const updatedList = list.map(t => {
-                        if(t.id == todo.id) {
-                            todo.finished = isFinished;
-                        }
-                        return t;
-                    });
-                    setList(updatedList);
-                }}
-
-                onDelete={() => {
-                  const updatedList = list.filter(t => t.id != todo.id) 
-                  setList(updatedList);
-                }}
-
-                onEdit={(todoText) => {
-                  const updatedList = list.map(t => {
-                        if(t.id == todo.id) {
-                            todo.todoData = todoText;
-                        }
-                        return t;
-                    });
-                    setList(updatedList);
-                }}
+              changeFinished={(isFinished) => onFinished(todo, isFinished)}
+              onDelete={() => onDelete(todo)}
+              onEdit={(todoText) => onEdit(todo, todoText)}
             />
       ))}
     </div>
