74490547994007f08f5cd22ae0ab1a47994157fd
Ankush2107 <ankush.kumar210701@gmail.com>
2024-01-15 09:53:13 +0530

    added edit and save functionality

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,18 +1,29 @@
 import { useState } from "react";
 
-const Todo = ({ todoData, isFinished, changeFinished }) => {
+const Todo = ({ todoData, isFinished, changeFinished, onDelete, onEdit }) => {
     const [ finished, setFinished ] = useState(isFinished);
+    const [ isEditting, setIsEditting ] = useState(false);
+    const [ editText, setEditText ] = useState(todoData);
   return (
     <div>
         <input type="checkbox" checked={finished} onChange={(e) => {
             setFinished(e.target.checked) // Updates the local finished state with the new checked state of the checkbox I mean superficially.
-
             changeFinished(e.target.checked) // Internally updates the existing finished value.
         }}  
         />
-        { todoData }
-        <button>Edit</button>
-        <button>Delete</button>
+
+        {/* TodoData */}
+        {(isEditting) ? <input type="text" 
+                               value={editText} 
+                               onChange={(e) => setEditText(e.target.value)} 
+                        /> : <span>{todoData}</span> }
+ 
+        <button onClick={() => {setIsEditting(!isEditting)
+                                onEdit(editText)
+                                }}>{(!isEditting) ? 'Edit' : 'Save'}
+        </button>
+
+        <button onClick={onDelete}>Delete</button>
     </div>
   )
 }
